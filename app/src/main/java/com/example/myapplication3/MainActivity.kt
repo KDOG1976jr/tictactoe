502b7e13e6c670413637ed61857b5317136d752e
Matt <140817553+KDOG1976jr@users.noreply.github.com>
2024-05-23 08:42:17 -0400

    I like cows

--- app/src/main/java/com/example/myapplication3/MainActivity.kt
+++ app/src/main/java/com/example/myapplication3/MainActivity.kt
@@ -40,27 +40,41 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkForWin(): Boolean {
-        TODO("Not yet implemented")
-        var textArray : Array<CharSequence>
+        val textArray : Array<CharSequence> = arrayOf(
+            findViewById<TextView>(R.id.textView0).text,
+            findViewById<TextView>(R.id.textView1).text,
+            findViewById<TextView>(R.id.textView2).text,
+            findViewById<TextView>(R.id.textView3).text,
+            findViewById<TextView>(R.id.textView4).text,
+            findViewById<TextView>(R.id.textView5).text,
+            findViewById<TextView>(R.id.textView6).text,
+            findViewById<TextView>(R.id.textView7).text,
+            findViewById<TextView>(R.id.textView8).text,
+        )
 
         return checkRowForWin(textArray) || checkColForWin(textArray) || checkLeftDiagForWin(textArray) || checkRightDiagForWin(textArray)
     }
 
     private fun checkRightDiagForWin(textArray: Array<CharSequence>): Boolean {
-        TODO("Not yet implemented")
+        return (textArray[2] == textArray[4] && textArray[4] == textArray[6] && textArray[0] != "");
     }
 
     private fun checkLeftDiagForWin(textArray: Array<CharSequence>): Boolean {
-        TODO("Not yet implemented")
+        return (textArray[0] == textArray[4] && textArray[4] == textArray[8] && textArray[0] != "");
     }
 
     private fun checkColForWin(textArray: Array<CharSequence>): Boolean {
-        TODO("Not yet implemented")
+        for(col in 0..2){
+            if(textArray[col + 0] == textArray[col + 3] && textArray[col + 3] == textArray[col + 6] && textArray[col] != ""){
+                return true
+            }
+        }
+        return false
     }
 
     private fun checkRowForWin(textArray: Array<CharSequence>): Boolean {
         for(row in 0..2){
-            if(textArray[3 * row + 0] == textArray[3 * row + 1] && textArray[3 * row + 1] == textArray[3 * row + 2]){
+            if(textArray[3 * row + 0] == textArray[3 * row + 1] && textArray[3 * row + 1] == textArray[3 * row + 2] && textArray[3*row] != ""){
                 return true
             }
         }
@@ -68,16 +82,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun handleWin() {
-        TODO("Not yet implemented")
-        TODO("Create a TextView variable called winLoseText and get from findViewById(R.id.winLose) ")
-        TODO("set the text of winLoseText to "$currentMove Wins!"")
+        val winLoseText = findViewById<TextView>(R.id.winlose)
+        winLoseText.text = "$currentMove Wins!"
     }
 
     fun topLeftClick(view: View) {
         val textView : TextView = findViewById(R.id.textView0)
         if (textView.text == ""){
             textView.text = currentMove
-            changeCurrentMove()
+            handleMove()
         }
     }
 
@@ -85,7 +98,7 @@ class MainActivity : AppCompatActivity() {
         val textView : TextView = findViewById(R.id.textView1)
         if (textView.text == ""){
             textView.text = currentMove
-            changeCurrentMove()
+            handleMove()
         }
     }
 }
